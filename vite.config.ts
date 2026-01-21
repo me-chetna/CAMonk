54b11a57c5f469e9c7ae3916b3aef6736fb15536
me-chetna <chetna.jain.ug24@nsut.ac.in>
2026-01-21 22:31:35 +0530

    Add

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
-  base : "/CAMonk/",
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: "/"
 })
